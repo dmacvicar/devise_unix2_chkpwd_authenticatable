55df7a901bdc546f6f42568f427fd269ea85bda5
Duncan Mac-Vicar P <dmacvicar@suse.de>
2011-11-21 12:23:01 +0100

    typo during search replace

--- lib/devise_unix2_chkpwd_authenticatable.rb
+++ lib/devise_unix2_chkpwd_authenticatable.rb
@@ -5,4 +5,4 @@ $: << File.expand_path("..", __FILE__)
 require 'devise_unix2_chkpwd_authenticatable/model'
 require 'devise_unix2_chkpwd_authenticatable/strategy'
 require 'devise_unix2_chkpwd_authenticatable/routes'
-Devise.add_module(:unix2_chkpwd_authenticatable, :strategy => true, :model => "devise_unix_authenticatable/model", :route => true)
+Devise.add_module(:unix2_chkpwd_authenticatable, :strategy => true, :model => "devise_unix2_chkpwd_authenticatable/model", :route => true)
