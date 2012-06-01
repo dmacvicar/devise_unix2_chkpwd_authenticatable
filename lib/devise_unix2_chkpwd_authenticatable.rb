349bb3a3005cdb38be65f892ecb8f8dc9b3458a1
Josef Reidinger <jreidinger@suse.cz>
2012-06-01 18:51:08 +0300

    do not modify load path in library ( slow down loading of other gems ). Instead use absolute path.

--- lib/devise_unix2_chkpwd_authenticatable.rb
+++ lib/devise_unix2_chkpwd_authenticatable.rb
@@ -1,8 +1,7 @@
 require 'devise'
 
-$: << File.expand_path("..", __FILE__)
+%w(model strategy routes).each do |mod|
+  require File.expand_path "../devise_unix2_chkpwd_authenticatable/#{mod}",__FILE__
+end
 
-require 'devise_unix2_chkpwd_authenticatable/model'
-require 'devise_unix2_chkpwd_authenticatable/strategy'
-require 'devise_unix2_chkpwd_authenticatable/routes'
 Devise.add_module(:unix2_chkpwd_authenticatable, :strategy => true, :model => "devise_unix2_chkpwd_authenticatable/model", :route => true)
