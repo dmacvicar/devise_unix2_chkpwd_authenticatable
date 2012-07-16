03c87c0355e9af16a565771ce8f4219cab7a538a
Ladislav Slezak <lslezak@novell.com>
2012-07-16 09:30:26 +0200

    added HTTP Basic authentication support

--- lib/devise_unix2_chkpwd_authenticatable/strategy.rb
+++ lib/devise_unix2_chkpwd_authenticatable/strategy.rb
@@ -5,13 +5,23 @@ module Devise
     class Unix2ChkpwdAuthenticatable < Base
 
       def valid?
-        valid = valid_params? && mapping.to.respond_to?(:authenticate_with_unix2_chkpwd)
+        valid = (valid_params? || valid_http_auth?) && mapping.to.respond_to?(:authenticate_with_unix2_chkpwd)
         Rails.logger.debug "valid?: #{valid}"
         valid
       end
 
       def authenticate!
-        if resource = mapping.to.authenticate_with_unix2_chkpwd(params[scope])
+        credentials = params[scope]
+
+        # credentials are empty when using HTTP auth
+        if credentials.nil?
+          # only HTTP Basic authentication is supported
+          request.authorization =~ /^Basic (.*)/m
+          usrname, pwd = Base64.decode64($1).split(/:/, 2)
+          credentials = {:username => usrname, :password => pwd}
+        end
+
+        if resource = mapping.to.authenticate_with_unix2_chkpwd(credentials)
           Rails.logger.error "*** Success!"
           success!(resource)
         else
@@ -30,6 +40,10 @@ module Devise
           params[scope] && params[scope][:password].present?
         end
 
+        def valid_http_auth?
+          # HTTP authentication enabled and credentials present in the request
+          mapping.to.http_authenticatable?(:unix2chkpwd) && request.authorization && request.authorization.match(/^Basic (.*)/)
+        end
     end
   end
 end
